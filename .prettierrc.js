240a587c818c1cc9abb5c7491117ead8af5293f1
Arslan Ahmad <47880584+de-ar@users.noreply.github.com>
2021-06-26 18:08:04 +0500

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'ignore',
+}
